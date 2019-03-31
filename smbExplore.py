8a5f63fb3954d7fa112954095481772f20cbc3e6
VFPAP <25368099+VFPAP@users.noreply.github.com>
2019-03-31 18:37:40 +0100

    dos2unix

--- smbExplore.py
+++ smbExplore.py
@@ -8,29 +8,29 @@ creds = input("Creds? (y/n)")
 
 if creds == 'y':
 
-	user = input("User:> ")
-	passwd = input("Password:> ")
-	domain = input("Domain:> ")
+        user = input("User:> ")
+        passwd = input("Password:> ")
+        domain = input("Domain:> ")
 
-	#print("\nListing folders...\n")
-	#os.system("smbclient -L " + ip + " -U "+user_passwd+" -W "+domain)
+        #print("\nListing folders...\n")
+        #os.system("smbclient -L " + ip + " -U "+user_passwd+" -W "+domain)
 
-	os.system("smbmap -u " +user+ " -p " +passwd+ " -H " +ip)
+        os.system("smbmap -u " +user+ " -p " +passwd+ " -H " +ip)
 
-	sn = input("\n\nSharename:> ")
+        sn = input("\n\nSharename:> ")
 
-	print("\n\nFolder >" + sn)
-	os.system("smbclient //" + ip + "/" + sn + " -U "+user_passwd+" -W "+domain)
+        print("\n\nFolder >" + sn)
+        os.system("smbclient //" + ip + "/" + sn + " -U "+user_passwd+" -W "+domain)
 
 elif creds == 'n':
 
-	#print("Listing folders...")
-	#os.system("smbclient -L " + ip + " -N")
+        #print("Listing folders...")
+        #os.system("smbclient -L " + ip + " -N")
 
-	os.system("smbmap -H " + ip)
+        os.system("smbmap -H " + ip)
 
 
-	sn = input("\nSharename:> ")
+        sn = input("\nSharename:> ")
 
-	print("\nFolder >" + sn)
-	os.system("smbclient //" + ip + "/" + sn +" -N")
+        print("\nFolder >" + sn)
+        os.system("smbclient //" + ip + "/" + sn +" -N")
